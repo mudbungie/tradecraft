5ef32e00819a5eeb9060fe136eff72e4348cb0f4
mudbungie <mudbungie@gmail.com>
2017-05-01 08:12:30 +0000

    nginx conf

--- server.py
+++ server.py
@@ -3,3 +3,13 @@
 # Actual application server. Answers calls from clients, according to API.
 
 import bottle
+
+@bottle.route('/')
+def index():
+    return 'True'
+
+def run_server():
+    bottle.run(host='127.0.0.1', port=8000)
+
+if __name__ == '__main__':
+    run_server()
