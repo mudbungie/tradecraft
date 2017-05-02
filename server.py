01c0eefe099cbb443f6b75e2db4e1e38f5f6d007
mudbungie <mudbungie@gmail.com>
2017-05-02 22:02:28 +0000

    registration and login in webserver

--- server.py
+++ server.py
@@ -2,28 +2,43 @@
 
 # Actual application server. Answers calls from clients, according to API.
 
-import bottle
+from bottle import get, post, request
 from tradecraft import api
 
-@bottle.route('/')
-def index():
-    return 'True'
+### Utility
+# Just gets values corresponding to keys from request.
+def get_values(request, keys):
+    values = []
+    for key in keys:
+        values.append(request.forms.get(key))
+    return values
 
+
+#
 # Registration functions.
+#
 @bottle.post('/register')
 def register():
-    email = bottle.request.forms.get('email')
+    email = get_values(request, ['email'])
     return json.dumps(api.account_exists(email))
 
 @bottle.post('/register/new')
 def register_new():
-    email = bottle.request.forms.get('email')
-    password = bottle.request.forms.get('password')
+    email, password = get_values(request, ['email', 'password'])
     registered = api.register(email, password): # Does validity checking.
     if registered:
         api.send_confirmation(email)
-    return registered
-    
+    return json.dumps(registered)
+
+@bottle.post
+def login():
+    email, password = get_values(request, ['email', 'password'])
+    return api.login(email, password) # Gives a token
+
+@get('/')
+def server_online():
+    return json.dumps(True)
+
 def run_server():
     bottle.run(host='127.0.0.1', port=8000)
 
